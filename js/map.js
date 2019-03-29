2eb5e1074bb706821b1b3de2e251cdcf7fcbf4ca
Andy Garcia <andreagarcia_sanchez@hotmail.com>
2019-03-28 20:20:03 -0600

    Visualizacion de mapa con ubicacion actual

---
+++ js/map.js
@@ -0,0 +1,22 @@
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (objPosition) {
+        // Dibuja mapa inicial con marcador de posicion
+        let defaultLayers = platform.createDefaultLayers();
+        let map = new H.Map(document.getElementById('mapContainer'),
+            defaultLayers.normal.map,
+            {
+                zoom: 14.5,
+                center: {
+                    lat: objPosition.coords.latitude,
+                    lng: objPosition.coords.longitude
+                }
+            });
+        let icon = new H.map.Icon('../assets/location.png');
+        let marker = new H.map.Marker({
+            lat: objPosition.coords.latitude,
+            lng: objPosition.coords.longitude
+        },
+            { icon: icon });
+        map.addObject(marker);
+    })
+}
